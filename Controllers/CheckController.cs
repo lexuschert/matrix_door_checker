b82e901476443d5665798c54ba8e82e59d251c5b
Aleksey Yakimov <lexuschert@gmail.com>
2023-03-29 16:15:50 +0400

    Init project, Checker controller

---
+++ Controllers/CheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Matrix.Controllers;
+
+public class CheckController : AbstractBaseController
+{
+    [HttpGet, HttpPost]
+    public IActionResult Index()
+    {
+
+        var request = Request;
+        
+        return Ok();
+    }
+}
